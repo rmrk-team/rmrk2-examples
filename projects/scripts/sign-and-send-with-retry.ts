893c9362c22d059b925cf3a4893986e73c9889b0
Yuri <yuripetusko@gmail.com>
2022-10-05 16:41:15 +0100

    Update sign-and-send-with-retry.ts

--- projects/scripts/sign-and-send-with-retry.ts
+++ projects/scripts/sign-and-send-with-retry.ts
@@ -26,79 +26,77 @@ interface ISendTxReturnType {
  * @param retry - retry count in case of failure.
  */
 export const signAndSendWithRetry = async (
-  tx: SubmittableExtrinsic<'promise', ISubmittableResult>,
-  account: KeyringPair,
-  resolvedOnFinalizedOnly = true,
-  retry = 0,
+    tx: SubmittableExtrinsic<'promise', ISubmittableResult>,
+    account: KeyringPair,
+    resolvedOnFinalizedOnly = true,
+    retry = 0,
 ): Promise<ISendTxReturnType> => {
   return new Promise(async (resolve, reject) => {
     const api = await getApi();
 
-    const returnObject: ISendTxReturnType = {
-      success: false,
-      hash: undefined,
-      included: [],
-      finalized: [],
-      block: 0,
-    };
+    const returnObject: ISendTxReturnType = { success: false, hash: undefined, included: [], finalized: [], block: 0 }
 
     try {
       const unsubscribe = await tx.signAndSend(
-        account,
-        { nonce: -1 },
-        async ({ events = [], status, dispatchError }) => {
-          returnObject.success = !dispatchError;
-          returnObject.included = [...events];
-          returnObject.hash = status.hash;
+          account,
+          { nonce: -1 },
+          async ({ events = [], status, dispatchError }) => {
+            returnObject.success = !dispatchError;
+            returnObject.included = [...events];
+            returnObject.hash = status.hash;
 
-          const rejectPromise = (error: any) => {
-            console.error(`Error sending tx`, error);
-            console.error(`tx for the error above`, tx.toHuman());
-            unsubscribe();
-            reject(error);
-          };
+            const rejectPromise = (error: any) => {
+              console.error(`Error sending tx`, error);
+              console.log(`tx for the error above`, tx.toHuman());
+              unsubscribe();
+              reject(error);
+            }
 
-          if (status.isInBlock) {
-            console.log(
-              `📀 Transaction ${tx.meta.name} included at blockHash ${
-                status.asInBlock
-              } [success = ${!dispatchError}]`,
-            );
+            if (status.isInBlock) {
+              console.log(
+                  `📀 Transaction ${tx.meta.name} included at blockHash ${status.asInBlock} [success = ${!dispatchError}]`,
+              );
 
-            // Get block number that this tx got into, to return back to user
-            const signedBlock = await api.rpc.chain.getBlock(status.asInBlock);
-            returnObject.block = signedBlock.block.header.number.toNumber();
+              // Get block number that this tx got into, to return back to user
+              const signedBlock = await api.rpc.chain.getBlock(status.asInBlock);
+              returnObject.block = signedBlock.block.header.number.toNumber();
 
-            // If we don't care about waiting for this tx to get into a finalized block, we can return early.
-            if (!resolvedOnFinalizedOnly && !dispatchError) {
+              // If we don't care about waiting for this tx to get into a finalized block, we can return early.
+              if (!resolvedOnFinalizedOnly && !dispatchError) {
+                unsubscribe();
+                resolve(returnObject);
+              }
+            } else if (status.isBroadcast) {
+              console.log(`🚀 Transaction broadcasted.`);
+            } else if (status.isFinalized) {
+              console.log(
+                  `💯 Transaction ${tx.meta.name}(..) Finalized at blockHash ${status.asFinalized}`,
+              );
+              if (returnObject.block === 0) {
+                const signedBlock = await api.rpc.chain.getBlock(status.asInBlock);
+                returnObject.block = signedBlock.block.header.number.toNumber();
+              }
+
               unsubscribe();
               resolve(returnObject);
+            } else if (status.isReady) {
+              // let's not be too noisy..
+            } else if (status.isInvalid) {
+              rejectPromise(new Error(`Extrinsic isInvalid`))
+            } else {
+              console.log(`🤷 Other status ${status}`);
             }
-          } else if (status.isBroadcast) {
-            console.log(`🚀 Transaction broadcasted.`);
-          } else if (status.isFinalized) {
-            console.log(
-              `💯 Transaction ${tx.meta.name}(..) Finalized at blockHash ${status.asFinalized}`,
-            );
-            unsubscribe();
-            resolve(returnObject);
-          } else if (status.isReady) {
-            // let's not be too noisy..
-          } else if (status.isInvalid) {
-            rejectPromise(new Error(`Extrinsic isInvalid`));
-          } else {
-            console.log(`🤷 Other status ${status}`);
-          }
-        },
+          },
       );
     } catch (error: any) {
       console.log(
-        `Error sending tx. Error: "${error.message}". TX: ${JSON.stringify(tx.toHuman())}`,
+          `Error sending tx. Error: "${error.message}". TX: ${JSON.stringify(tx.toHuman())}`,
       );
       if (retry < MAX_RETRIES) {
         console.log(`sendAndFinalize Retry #${retry} of ${MAX_RETRIES}`);
         await sleep(RETRY_DELAY_SECONDS * 1000);
-        return await signAndSendWithRetry(tx, account, resolvedOnFinalizedOnly, retry + 1);
+        const result = await signAndSendWithRetry(tx, account, resolvedOnFinalizedOnly, retry + 1);
+        resolve(result);
       } else {
         console.error(`Error initiating tx signAndSend`, error);
         reject(error);
